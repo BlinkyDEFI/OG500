9661329f8910170a119d1bbbc4a007c417318766
BlinkyDEFI <blinkyonsol@gmail.com>
2025-05-29 16:41:00 +0200

    Update WalletStatus.tsx

--- src/components/mint/WalletStatus.tsx
+++ src/components/mint/WalletStatus.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { WalletMultiButton } from "@/components/WalletMultiButton";
 
@@ -14,8 +13,15 @@ export const WalletStatus: React.FC<WalletStatusProps> = ({ connected }) => {
       </div>
       
       {!connected && (
-        <div className="text-center text-green-400 py-4">
-          Connect your wallet to mint.
+        <div className="space-y-3">
+          <div className="text-center text-green-400 py-2">
+            Connect your wallet to mint.
+          </div>
+          <div className="p-3 bg-gray-800 rounded-md border border-gray-700">
+            <p className="text-xs text-gray-400 text-center">
+              Phantom and Solflare wallets have been tested. Other wallets may appear as options but are used at your own risk.
+            </p>
+          </div>
         </div>
       )}
     </>
